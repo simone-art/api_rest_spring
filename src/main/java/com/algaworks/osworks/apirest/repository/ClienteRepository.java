bd802059828441fdbed753b78946fcad04c455ad
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-03-12 19:17:38 -0300

    "Criado métood que busca o cliente pelo id"

--- src/main/java/com/algaworks/osworks/apirest/repository/ClienteRepository.java
+++ src/main/java/com/algaworks/osworks/apirest/repository/ClienteRepository.java
@@ -14,6 +14,6 @@ import java.util.List;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long > {
-     //List<Cliente> findByNome(String nome);
-     List<Cliente> findByNomeContaining(String nome);
+     List<Cliente> findByNome(String nome);
+     //List<Cliente> findByNomeContaining(String nome);
 }

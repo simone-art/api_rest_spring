c5a0c725a5989159b31a5d91d847702b4aeb160b
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-03-12 11:38:54 -0300

    "Criada variável de instância no controle para fazer consultas na tabela"

--- src/main/java/com/algaworks/osworks/apirest/controller/ClienteController.java
+++ src/main/java/com/algaworks/osworks/apirest/controller/ClienteController.java
@@ -4,6 +4,8 @@ import com.algaworks.osworks.apirest.model.Cliente;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -12,19 +14,17 @@ import java.util.List;
 @RestController
 public class ClienteController {
 
-    @GetMapping("/clientes")
-    public List<Cliente> listar(){
-        var cliente1 = new Cliente();
-        var cliente2 = new Cliente();
-        cliente1.setId(1L);
-        cliente1.setNome("Simone");
-        cliente1.setEmail("simonesantos@gmail.com");
-        cliente1.setTelefone("11 9999 8888");
+    //Variável de instância que permite
+    //EntityManager é uma interfase do Jakarta Persistence que é usada pra fzer operações nas entidades
+    //Como consulta, criação, exclusão, etc.
+    //Pra o EntityManager estar disponível, ele deve ser instânciado e injetado no controller por medio
+    //Da anotação @PersistenceContext
 
-        cliente2.setId(2L);
-        cliente2.setNome("Polo");
-        cliente2.setEmail("beltrandpolo56@gmail.com");
-        cliente2.setTelefone("11 9999 7777");
-        return Arrays.asList(cliente1, cliente2);
-    }
+    @PersistenceContext
+    private EntityManager entityManager;
+
+//    @GetMapping("/clientes")
+//    public List<Cliente> listar(){
+//
+//    }
 }

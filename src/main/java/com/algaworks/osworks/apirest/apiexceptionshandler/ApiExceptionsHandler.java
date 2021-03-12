bc6a427ff4843e1af814abf8cc3c13582c6313b3
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-03-12 20:57:12 -0300

    "Criada classe pra tratar exclusivamente de exceptions"

---
+++ src/main/java/com/algaworks/osworks/apirest/apiexceptionshandler/ApiExceptionsHandler.java
@@ -0,0 +1,22 @@
+package com.algaworks.osworks.apirest.apiexceptionshandler;
+
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+//Anotação pra tratativa de exceptions
+//ResponseEntityExceptionHandler tem embutido vários tratamentos básico de exceptions
+
+@ControllerAdvice
+public class ApiExceptionsHandler extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        return super.handleMethodArgumentNotValid(ex, headers, status, request);
+    }
+}

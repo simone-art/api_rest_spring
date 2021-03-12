07509278e85cac595e2c33a47d4ab3e24423fb19
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-03-12 18:49:09 -0300

    "Usado o return clienteRepository.findByNomeContaining("mo") no método que lista os clientes"

--- src/main/java/com/algaworks/osworks/apirest/controller/ClienteController.java
+++ src/main/java/com/algaworks/osworks/apirest/controller/ClienteController.java
@@ -22,6 +22,6 @@ public class ClienteController {
     @GetMapping("/clientes")
     public List<Cliente> listar(){
      //return clienteRepository.findAll();
-      return clienteRepository.findByNome("Simone Santos");
+      return clienteRepository.findByNomeContaining("mo");
     }
 }

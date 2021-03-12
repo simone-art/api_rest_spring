f0f9c6d75c7562034e8d71d9b83dac8b82512ab0
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-03-12 19:23:41 -0300

    "Criado método que busca o cliente pelo id mas usando o ResponseEntity"

--- src/main/java/com/algaworks/osworks/apirest/controller/ClienteController.java
+++ src/main/java/com/algaworks/osworks/apirest/controller/ClienteController.java
@@ -3,6 +3,7 @@ package com.algaworks.osworks.apirest.controller;
 import com.algaworks.osworks.apirest.model.Cliente;
 import com.algaworks.osworks.apirest.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,8 +31,12 @@ public class ClienteController {
     //OPTIONAL é um container que pode estar nulo ou conter algum dado
     //O optional extrai o que está dentro do cliente e se não tiver nada retorna nulo
     @GetMapping("/clientes/{clienteId}")
-    public Cliente buscar(@PathVariable Long clienteId){
+    public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId){
        Optional<Cliente>  cliente = clienteRepository.findById(clienteId);
-        return cliente.orElse(null);
+
+       if(cliente.isPresent()){
+           return ResponseEntity.ok(cliente.get());
+       }
+        return ResponseEntity.notFound().build();
     }
 }

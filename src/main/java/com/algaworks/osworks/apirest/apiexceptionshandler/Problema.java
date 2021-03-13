7b3d2246b6de3dabef96dbf5802e5dc0f4e12096
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-03-12 21:02:20 -0300

    "Criada classe com atributos novos pra mostrar mensagem quando um dado vier nullo, em branco o em a configuração respectiva"

---
+++ src/main/java/com/algaworks/osworks/apirest/apiexceptionshandler/Problema.java
@@ -0,0 +1,39 @@
+package com.algaworks.osworks.apirest.apiexceptionshandler;
+
+import java.time.LocalDateTime;
+
+public class Problema {
+
+    private Integer status;
+    private LocalDateTime dataHora;
+    private String titulo;
+
+    /***
+     *
+     * Criado getters and setters
+     */
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getDataHora() {
+        return dataHora;
+    }
+
+    public void setDataHora(LocalDateTime dataHora) {
+        this.dataHora = dataHora;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+}

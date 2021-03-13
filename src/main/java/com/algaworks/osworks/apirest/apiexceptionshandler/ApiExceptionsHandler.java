0cf54a5f09e45c2d2b23b27f0eb229ef2f6dc30a
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-03-12 21:07:24 -0300

    "Instancia a classe problema dentro deste arqivo pra setar as mensagens correspondentes"

--- src/main/java/com/algaworks/osworks/apirest/apiexceptionshandler/ApiExceptionsHandler.java
+++ src/main/java/com/algaworks/osworks/apirest/apiexceptionshandler/ApiExceptionsHandler.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.time.LocalDateTime;
+
 //Anotação pra tratativa de exceptions
 //ResponseEntityExceptionHandler tem embutido vários tratamentos básico de exceptions
 
@@ -17,6 +19,13 @@ public class ApiExceptionsHandler extends ResponseEntityExceptionHandler {
 
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        return super.handleMethodArgumentNotValid(ex, headers, status, request);
+
+        var problema = new Problema();
+        problema.setStatus(status.value());
+        problema.setTitulo("Um ou mais campos estão inválidos. " +
+                "Faça o preenchimento correto e tente novamente");
+        problema.setDataHora(LocalDateTime.now());
+
+        return super.handleExceptionInternal(ex, problema, headers, status, request);
     }
 }

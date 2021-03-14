4f973cd9e7dc5ab396733980267f15767fa23406
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-03-14 14:23:22 -0300

    "Definido método para criar uma ordem de servico usando o endpoint /ordens-servico"

--- src/main/java/com/algaworks/osworks/apirest/controller/OrdemServicoController.java
+++ src/main/java/com/algaworks/osworks/apirest/controller/OrdemServicoController.java
@@ -1,10 +1,23 @@
 package com.algaworks.osworks.apirest.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.algaworks.osworks.apirest.model.OrdemServico;
+import com.algaworks.osworks.apirest.service.GestaoOrdemServicoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/ordens-servico")
 public class OrdemServicoController {
+
+    @Autowired
+    private GestaoOrdemServicoService gestaoOrdemServicoService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public OrdemServico criar(@RequestBody OrdemServico ordemServico){
+        return gestaoOrdemServicoService.criar(ordemServico);
+
+    }
 
 }

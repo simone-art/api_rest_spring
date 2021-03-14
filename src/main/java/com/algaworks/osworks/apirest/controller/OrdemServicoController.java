3ee3f7ac73d05e95941ca5aab13b82a6b29b918d
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-03-14 17:04:08 -0300

    "Criado método que lista todas as ordem de serviço"

--- src/main/java/com/algaworks/osworks/apirest/controller/OrdemServicoController.java
+++ src/main/java/com/algaworks/osworks/apirest/controller/OrdemServicoController.java
@@ -1,12 +1,14 @@
 package com.algaworks.osworks.apirest.controller;
 
 import com.algaworks.osworks.apirest.model.OrdemServico;
+import com.algaworks.osworks.apirest.repository.OrdemServicoRepository;
 import com.algaworks.osworks.apirest.service.GestaoOrdemServicoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/ordens-servico")
@@ -15,12 +17,19 @@ public class OrdemServicoController {
     @Autowired
     private GestaoOrdemServicoService gestaoOrdemServicoService;
 
+    @Autowired
+    private OrdemServicoRepository ordemServicoRepository;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public OrdemServico criar(@Valid @RequestBody OrdemServico ordemServico){
         return gestaoOrdemServicoService.criar(ordemServico);
 
+    }
+
+    @GetMapping
+    public List<OrdemServico> listar(){
+        return ordemServicoRepository.findAll();
     }
 
 }

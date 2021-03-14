fad8fbbcbc3cb1c60b106406f16a7178d05dc3c9
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-03-14 13:10:45 -0300

    "Criados getters and setters"

--- src/main/java/com/algaworks/osworks/apirest/model/OrdemDeServico.java
+++ src/main/java/com/algaworks/osworks/apirest/model/OrdemDeServico.java
@@ -19,4 +19,59 @@ public class OrdemDeServico {
      * Criação dos getters and setters
      */
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+
+    public StatusOrdemDeServico getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatusOrdemDeServico status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getDataAbertura() {
+        return dataAbertura;
+    }
+
+    public void setDataAbertura(LocalDateTime dataAbertura) {
+        this.dataAbertura = dataAbertura;
+    }
+
+    public LocalDateTime getDataFinalizacao() {
+        return dataFinalizacao;
+    }
+
+    public void setDataFinalizacao(LocalDateTime dataFinalizacao) {
+        this.dataFinalizacao = dataFinalizacao;
+    }
 }

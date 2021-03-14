a125e1a560f0eb0eda1b2c674fdb0e057b62af47
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-03-14 14:17:33 -0300

    "Criada classe pra mapear os métodos dos endpoints"

---
+++ src/main/java/com/algaworks/osworks/apirest/controller/OrdemServicoController.java
@@ -0,0 +1,10 @@
+package com.algaworks.osworks.apirest.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ordens-servico")
+public class OrdemServicoController {
+
+}

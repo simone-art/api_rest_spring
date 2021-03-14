ddf16c46f0b64720f234393bdcdbc3ab984f79c5
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-03-14 11:02:46 -0300

    "Adicionado código que mostra mensagem de erro ao usuário que não preencheu todos os dados"

--- src/main/java/com/algaworks/osworks/apirest/apiexceptionshandler/ApiExceptionsHandler.java
+++ src/main/java/com/algaworks/osworks/apirest/apiexceptionshandler/ApiExceptionsHandler.java
@@ -4,15 +4,20 @@ package com.algaworks.osworks.apirest.apiexceptionshandler;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 //Anotação pra tratativa de exceptions
 //ResponseEntityExceptionHandler tem embutido vários tratamentos básico de exceptions
+
+//((FieldError)error).getField(); pega o nome do campo
 
 @ControllerAdvice
 public class ApiExceptionsHandler extends ResponseEntityExceptionHandler {
@@ -20,10 +25,20 @@ public class ApiExceptionsHandler extends ResponseEntityExceptionHandler {
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
 
+        var campos = new ArrayList<Problema.Campo>();
+
+        for(ObjectError error: ex.getBindingResult().getAllErrors()){
+            String nome = ((FieldError)error).getField();
+            String mensagem = error.getDefaultMessage();
+
+            campos.add(new Problema.Campo(nome, mensagem));
+        }
+
         var problema = new Problema();
         problema.setStatus(status.value());
         problema.setTitulo("Um ou mais campos estão inválidos. Faça o preenchimento correto e tente novamente");
         problema.setDataHora(LocalDateTime.now());
+        problema.setCampos(campos);
 
         return super.handleExceptionInternal(ex, problema, headers, status, request);
     }

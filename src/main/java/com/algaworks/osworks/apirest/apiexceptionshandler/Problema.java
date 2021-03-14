4440dc43b566c4017d56c4b77a147a32f86d8859
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-03-14 12:49:43 -0300

    "Adicionad @JsonInclude pra incluir só campos que não estejam nulos"

--- src/main/java/com/algaworks/osworks/apirest/apiexceptionshandler/Problema.java
+++ src/main/java/com/algaworks/osworks/apirest/apiexceptionshandler/Problema.java
@@ -1,8 +1,15 @@
 package com.algaworks.osworks.apirest.apiexceptionshandler;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import java.time.LocalDateTime;
 import java.util.List;
 
+//@JsonInclude(JsonInclude.Include.NON_NULL), ao serializar em Json só inclua os campos que
+//não estejam nulos
+
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Problema {
 
     private Integer status;

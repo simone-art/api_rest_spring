dc29c67efe46db0828749ec38d2ebd054f99a9b5
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-03-14 17:12:25 -0300

    "Criado método que busca ordem pelo id"

--- src/main/java/com/algaworks/osworks/apirest/controller/OrdemServicoController.java
+++ src/main/java/com/algaworks/osworks/apirest/controller/OrdemServicoController.java
@@ -5,10 +5,12 @@ import com.algaworks.osworks.apirest.repository.OrdemServicoRepository;
 import com.algaworks.osworks.apirest.service.GestaoOrdemServicoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/ordens-servico")
@@ -31,5 +33,16 @@ public class OrdemServicoController {
     public List<OrdemServico> listar(){
         return ordemServicoRepository.findAll();
     }
+
+    @GetMapping("/{ordemServicoId}")
+    public ResponseEntity<OrdemServico> buscarPeloId(@PathVariable Long ordemServicoId){
+        Optional<OrdemServico> ordemServico = ordemServicoRepository.findById(ordemServicoId);
+        if (ordemServico.isPresent()){
+            return ResponseEntity.ok(ordemServico.get());
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+
 
 }

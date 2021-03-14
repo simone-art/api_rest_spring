3931b5440d47e2864ec491ff6e5ee755ee793430
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-03-14 13:14:23 -0300

    "Adicionado ID e o Generated.value"

--- src/main/java/com/algaworks/osworks/apirest/model/OrdemDeServico.java
+++ src/main/java/com/algaworks/osworks/apirest/model/OrdemDeServico.java
@@ -1,6 +1,9 @@
 package com.algaworks.osworks.apirest.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.Objects;
@@ -8,6 +11,8 @@ import java.util.Objects;
 @Entity
 public class OrdemDeServico {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Cliente cliente;
     private String descricao;
